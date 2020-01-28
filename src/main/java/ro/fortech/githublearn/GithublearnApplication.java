3e3ee345db9e36dbd345eb4d3d1fed14b1ec45a9
Ciprian Chise <ciprian.chise@fortech.ro>
2020-01-28 13:19:40 +0200

    getAllUser
    final commit getAllUsers

--- src/main/java/ro/fortech/githublearn/GithublearnApplication.java
+++ src/main/java/ro/fortech/githublearn/GithublearnApplication.java
@@ -1,13 +1,25 @@
 package ro.fortech.githublearn;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ro.fortech.githublearn.model.UserModel;
+import ro.fortech.githublearn.repository.UserRepository;
 
 @SpringBootApplication
 public class GithublearnApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GithublearnApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(GithublearnApplication.class, args);
+  }
+
+  @Bean
+  CommandLineRunner atStartup(UserRepository repo) {
+    return args -> {
+      repo.save(new UserModel("Jane", "Doe"));
+      repo.save(new UserModel("Bogosel", "Daniel"));
+    };
+  }
 
 }
--- src/main/java/ro/fortech/githublearn/service/UserService.java
+++ src/main/java/ro/fortech/githublearn/service/UserService.java
@@ -1,6 +1,9 @@
 package ro.fortech.githublearn.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Service;
+import ro.fortech.githublearn.model.UserModel;
 import ro.fortech.githublearn.repository.UserRepository;
 
 @Service
@@ -10,5 +13,10 @@ public class UserService {
 
   public UserService(UserRepository userRepository) {
     this.userRepository = userRepository;
+  }
+
+  public List<UserModel> getAllUsers() {
+    List<UserModel> userList = new ArrayList<>();
+    return userList = userRepository.findAll();
   }
 }
