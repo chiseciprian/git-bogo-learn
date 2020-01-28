2f63022ad3a5f8d49e9b4e4c9e71a49fb1c1dcb2
Ciprian Chise <ciprian.chise@fortech.ro>
2020-01-28 13:33:36 +0200

    addUser
    final commit for addUser

--- src/main/java/ro/fortech/githublearn/controller/UserController.java
+++ src/main/java/ro/fortech/githublearn/controller/UserController.java
@@ -1,7 +1,10 @@
 package ro.fortech.githublearn.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ro.fortech.githublearn.model.UserModel;
 import ro.fortech.githublearn.service.UserService;
 
 @RestController
@@ -12,5 +15,10 @@ public class UserController {
 
   public UserController(UserService userService) {
     this.userService = userService;
+  }
+
+  @PostMapping
+  public UserModel addUser(@RequestBody UserModel userModel){
+    return userService.addUser(userModel);
   }
 }
--- src/main/java/ro/fortech/githublearn/service/UserService.java
+++ src/main/java/ro/fortech/githublearn/service/UserService.java
@@ -1,6 +1,7 @@
 package ro.fortech.githublearn.service;
 
 import org.springframework.stereotype.Service;
+import ro.fortech.githublearn.model.UserModel;
 import ro.fortech.githublearn.repository.UserRepository;
 
 @Service
@@ -10,5 +11,11 @@ public class UserService {
 
   public UserService(UserRepository userRepository) {
     this.userRepository = userRepository;
+  }
+
+
+  public UserModel addUser(UserModel userModel){
+    userRepository.save(userModel);
+    return userModel;
   }
 }
