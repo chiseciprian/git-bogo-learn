47ed47cf3d3b2e4fccc6cbc895ec0d7cf031e2ae
Ciprian Chise <ciprian.chise@fortech.ro>
2020-01-28 10:51:11 +0200

    first commit

---
+++ src/main/java/ro/fortech/githublearn/GithublearnApplication.java
@@ -0,0 +1,13 @@
+package ro.fortech.githublearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithublearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithublearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/fortech/githublearn/GithublearnApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.fortech.githublearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithublearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
